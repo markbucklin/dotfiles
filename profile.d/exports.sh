b601e42a82dff1504c74c3c66c07adca617d78dc
Mark Bucklin <markbucklin@gmail.com>
2018-12-31 07:32:18 -0500

    add completion scripts from linuxbrew and others

--- profile.d/exports.sh
+++ profile.d/exports.sh
@@ -40,6 +40,16 @@ if [[ -x $(which less 2> /dev/null) ]]; then
     fi
 fi
 
+export PAGER='most'
+export LESS_TERMCAP_mb="$'\e[1;32m'"
+export LESS_TERMCAP_md="$'\e[1;32m'"
+export LESS_TERMCAP_me="$'\e[0m'"
+export LESS_TERMCAP_se="$'\e[0m'"
+export LESS_TERMCAP_so="$'\e[01;33m'"
+export LESS_TERMCAP_ue="$'\e[0m'"
+export LESS_TERMCAP_us="$'\e[1;4;31m'"
+
+
 # Set default editor
 # if [[ -x $(which  2> /dev/null) ]]; then
 export EDITOR=vim
