e61b282f94ea72d6b1543d66b26ac3b8eef28053
Mark Bucklin <markbucklin@gmail.com>
2019-01-19 07:35:42 -0500

    remove linuxbrew configuration and references

--- env.d/go.sh
+++ env.d/go.sh
@@ -1,7 +1,5 @@
 
 # todo: check path for go and install if necessary
-# todo: fix multiple go installs (/usr/local/go and ~/.linuxbrew)
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
-
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
