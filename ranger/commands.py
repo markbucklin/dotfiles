56a00ee2ec994152b87c0cfe3d7a6e294dcb06f8
Mark Bucklin <markbucklin@gmail.com>
2019-02-28 10:13:46 -0500

    add files from luksmithxyz dotfiles for ranger and vim

--- ranger/commands.py
+++ ranger/commands.py
@@ -4,23 +4,17 @@
 # documentation.  Do NOT add them all here, or you may end up with defunct
 # commands when upgrading ranger.
 
-
-
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import *
 
 # A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 # You can import any python module as needed.
 import os
 
 # Any class that is a subclass of "Command" will be integrated into ranger as a
 # command.  Try typing ":my_edit<ENTER>" in ranger!
-
-# set preview_images true
-
-
 class my_edit(Command):
     # The so-called doc-string of the class will be visible in the built-in
     # help that is accessible by typing "?c" inside ranger.
@@ -43,7 +37,7 @@ class my_edit(Command):
             # reference to the currently selected file.
             target_filename = self.fm.thisfile.path
 
-        # This is a generic function to print text in ranger.
+        # This is a generic function to print text in ranger.  
         self.fm.notify("Let's edit the file " + target_filename + "!")
 
         # Using bad=True in fm.notify allows you to print error messages:
@@ -58,13 +52,14 @@ class my_edit(Command):
 
     # The tab method is called when you press tab, and should return a list of
     # suggestions that the user will tab through.
-    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
-    def tab(self, tabnum):
+    def tab(self):
         # This is a generic tab-completion function that iterates through the
         # content of the current directory.
         return self._tab_directory_content()
 
 
+# https://github.com/ranger/ranger/wiki/Integrating-File-Search-with-fzf
+# Now, simply bind this function to a key, by adding this to your ~/.config/ranger/rc.conf: map <C-f> fzf_select
 class fzf_select(Command):
     """
     :fzf_select
@@ -77,7 +72,6 @@ class fzf_select(Command):
     """
     def execute(self):
         import subprocess
-        import os.path
         if self.quantifier:
             # match only directories
             command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
@@ -86,10 +80,10 @@ class fzf_select(Command):
             # match files and directories
             command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
             if os.path.isdir(fzf_file):
                 self.fm.cd(fzf_file)
             else:
@@ -120,262 +114,103 @@ class fzf_locate(Command):
             else:
                 self.fm.select_file(fzf_file)
 
-class mkcd(Command):
+class fzf_bring(Command):
     """
-    :mkcd <dirname>
+    :fzf_bring
 
-    Creates a directory with the name <dirname> and enters it.
-    """
+    Find a file using fzf and bring it to the current directory.
 
-    def execute(self):
-        from os.path import join, expanduser, lexists
-        from os import makedirs
-        import re
-
-        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
-        if not lexists(dirname):
-            makedirs(dirname)
-
-            match = re.search('^/|^~[^/]*/', dirname)
-            if match:
-                self.fm.cd(match.group(0))
-                dirname = dirname[match.end(0):]
-
-            for m in re.finditer('[^/]+', dirname):
-                s = m.group(0)
-                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
-                    self.fm.cd(s)
-                else:
-                    ## We force ranger to load content before calling `scout`.
-                    self.fm.thisdir.load_content(schedule=False)
-                    self.fm.execute_console('scout -ae ^{}$'.format(s))
-        else:
-            self.fm.notify("file/directory exists!", bad=True)
-
-class toggle_flat(Command):
-    """
-    :toggle_flat
-
-    Flattens or unflattens the directory view.
-    """
-
-    def execute(self):
-        if self.fm.thisdir.flat == 0:
-            self.fm.thisdir.unload()
-            self.fm.thisdir.flat = -1
-            self.fm.thisdir.load_content()
-        else:
-            self.fm.thisdir.unload()
-            self.fm.thisdir.flat = 0
-            self.fm.thisdir.load_content()
-
-
-class fasd(Command):
-    """
-    :fasd
-
-    Jump to directory using fasd
+    See: https://github.com/junegunn/fzf
     """
     def execute(self):
         import subprocess
-        arg = self.rest(1)
-        if arg:
-            directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
-            self.fm.cd(directory)
-
-from collections import deque
-fd_deq = deque()
-class fd_search(Command):
-    """:fd_search [-d<depth>] <query>
-
-    Executes "fd -d<depth> <query>" in the current directory and focuses the
-    first match. <depth> defaults to 1, i.e. only the contents of the current
-    directory.
-    """
-
-    def execute(self):
-        import subprocess
-        from ranger.ext.get_executables import get_executables
-        if not 'fd' in get_executables():
-            self.fm.notify("Couldn't find fd on the PATH.", bad=True)
-            return
-        if self.arg(1):
-            if self.arg(1)[:2] == '-d':
-                depth = self.arg(1)
-                target = self.rest(2)
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
             else:
-                depth = '-d1'
-                target = self.rest(1)
-        else:
-            self.fm.notify(":fd_search needs a query.", bad=True)
+                self.fm.select_file(fzf_file)
+
+
+import os
+from ranger.core.loader import CommandLoader
+
+class compress(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
             return
 
-        # For convenience, change which dict is used as result_sep to change
-        # fd's behavior from splitting results by \0, which allows for newlines
-        # in your filenames to splitting results by \n, which allows for \0 in
-        # filenames.
-        null_sep = {'arg': '-0', 'split': '\0'}
-        nl_sep = {'arg': '', 'split': '\n'}
-        result_sep = null_sep
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
 
-        process = subprocess.Popen(['fd', result_sep['arg'], depth, target],
-                    universal_newlines=True, stdout=subprocess.PIPE)
-        (search_results, _err) = process.communicate()
-        global fd_deq
-        fd_deq = deque((self.fm.thisdir.path + os.sep + rel for rel in
-            sorted(search_results.split(result_sep['split']), key=str.lower)
-            if rel != ''))
-        if len(fd_deq) > 0:
-            self.fm.select_file(fd_deq[0])
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
 
-class fd_next(Command):
-    """:fd_next
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
 
-    Selects the next match from the last :fd_search.
-    """
-
-    def execute(self):
-        if len(fd_deq) > 1:
-            fd_deq.rotate(-1) # rotate left
-            self.fm.select_file(fd_deq[0])
-        elif len(fd_deq) == 1:
-            self.fm.select_file(fd_deq[0])
-
-class fd_prev(Command):
-    """:fd_prev
-
-    Selects the next match from the last :fd_search.
-    """
-
-    def execute(self):
-        if len(fd_deq) > 1:
-            fd_deq.rotate(1) # rotate right
-            self.fm.select_file(fd_deq[0])
-        elif len(fd_deq) == 1:
-            self.fm.select_file(fd_deq[0])
-
-# map <alt>/ console fd_search -d5%space # Setting the depth to a different default, omit -d if you're fine with -d1
-# map <alt>n fd_next
-# map <alt>p fd_prev
-
-
-class ag(Command):
-    """:ag 'regex'
-    Looks for a string in all marked paths or current dir
-    """
-    editor = os.getenv('EDITOR') or 'vim'
-    acmd = 'ag --smart-case --group --color --hidden'  # --search-zip
-    qarg = re.compile(r"""^(".*"|'.*')$""")
-    patterns = []
-    # THINK:USE: set_clipboard on each direct ':ag' search? So I could find in vim easily
-
-    def _sel(self):
-        d = self.fm.thisdir
-        if d.marked_items:
-            return [f.relative_path for f in d.marked_items]
-        # WARN: permanently hidden files like .* are searched anyways
-        #   << BUG: files skipped in .agignore are grep'ed being added on cmdline
-        if d.temporary_filter and d.files_all and (len(d.files_all) != len(d.files)):
-            return [f.relative_path for f in d.files]
-        return []
-
-    def _arg(self, i=1):
-        if self.rest(i):
-            ag.patterns.append(self.rest(i))
-        return ag.patterns[-1] if ag.patterns else ''
-
-    def _quot(self, patt):
-        return patt if ag.qarg.match(patt) else shell_quote(patt)
-
-    def _bare(self, patt):
-        return patt[1:-1] if ag.qarg.match(patt) else patt
-
-    def _aug_vim(self, iarg, comm='Ag'):
-        if self.arg(iarg) == '-Q':
-            self.shift()
-            comm = 'sil AgSet def.e.literal 1|' + comm
-        # patt = self._quot(self._arg(iarg))
-        patt = self._arg(iarg)  # No need to quote in new ag.vim
-        # FIXME:(add support)  'AgPaths' + self._sel()
-        cmd = ' '.join([comm, patt])
-        cmdl = [ag.editor, '-c', cmd, '-c', 'only']
-        return (cmdl, '')
-
-    def _aug_sh(self, iarg, flags=[]):
-        cmdl = ag.acmd.split() + flags
-        if iarg == 1:
-            import shlex
-            cmdl += shlex.split(self.rest(iarg))
-        else:
-            # NOTE: only allowed switches
-            opt = self.arg(iarg)
-            while opt in ['-Q', '-w']:
-                self.shift()
-                cmdl.append(opt)
-                opt = self.arg(iarg)
-            # TODO: save -Q/-w into ag.patterns =NEED rewrite plugin to join _aug*()
-            patt = self._bare(self._arg(iarg))  # THINK? use shlex.split() also/instead
-            cmdl.append(patt)
-        if '-g' not in flags:
-            cmdl += self._sel()
-        return (cmdl, '-p')
-
-    def _choose(self):
-        if self.arg(1) == '-v':
-            return self._aug_vim(2, 'Ag')
-        elif self.arg(1) == '-g':
-            return self._aug_vim(2, 'sil AgView grp|Ag')
-        elif self.arg(1) == '-l':
-            return self._aug_sh(2, ['--files-with-matches', '--count'])
-        elif self.arg(1) == '-p':  # paths
-            return self._aug_sh(2, ['-g'])
-        elif self.arg(1) == '-f':
-            return self._aug_sh(2)
-        elif self.arg(1) == '-r':
-            return self._aug_sh(2, ['--files-with-matches'])
-        else:
-            return self._aug_sh(1)
-
-    def _catch(self, cmd):
-        from subprocess import check_output, CalledProcessError
-        try:
-            out = check_output(cmd)
-        except CalledProcessError:
-            return None
-        else:
-            return out[:-1].decode('utf-8').splitlines()
-
-    # DEV
-    # NOTE: regex becomes very big for big dirs
-    # BAD: flat ignores 'filter' for nested dirs
-    def _filter(self, lst, thisdir):
-        # filter /^rel_dir/ on lst
-        # get leftmost path elements
-        # make regex '^' + '|'.join(re.escape(nm)) + '$'
-        thisdir.temporary_filter = re.compile(file_with_matches)
-        thisdir.refilter()
-
-        for f in thisdir.files_all:
-            if f.is_directory:
-                # DEV: each time filter-out one level of files from lst
-                self._filter(lst, f)
-
-    def execute(self):
-        cmd, flags = self._choose()
-        # self.fm.notify(cmd)
-        # TODO:ENH: cmd may be [..] -- no need to shell_escape
-        if self.arg(1) != '-r':
-            self.fm.execute_command(cmd, flags=flags)
-        else:
-            self._filter(self._catch(cmd))
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
 
     def tab(self):
-        # BAD:(:ag <prev_patt>) when input alias ':agv' and then <Tab>
-        #   <= EXPL: aliases expanded before parsing cmdline
-        cmd = self.arg(0)
-        flg = self.arg(1)
-        if flg[0] == '-' and flg[1] in 'flvgprw':
-            cmd += ' ' + flg
-        return ['{} {}'.format(cmd, p) for p in reversed(ag.patterns)]
+        """ Complete with current folder name """
+
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
+
+
+
+
+import os
+from ranger.core.loader import CommandLoader
+
+class extracthere(Command):
+    def execute(self):
+        """ Extract copied files to current directory """
+        copied_files = tuple(self.fm.copy_buffer)
+
+        if not copied_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = copied_files[0]
+        cwd = self.fm.thisdir
+        original_path = cwd.path
+        au_flags = ['-X', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        if len(copied_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags \
+                + [f.path for f in copied_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
 
 
