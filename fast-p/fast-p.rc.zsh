3ac370fe3d25c49fd82dc4adc730cae48a5e86bf
Mark Bucklin <markbucklin@gmail.com>
2019-03-13 12:54:04 -0400

    add current evervim custom functions

--- fast-p/fast-p.rc.zsh
+++ fast-p/fast-p.rc.zsh
@@ -34,3 +34,15 @@ fzf-select-pdf-print-path () {
 }
 
 # 
+p () {
+    open=zathura
+
+    ag -U -g ".pdf$" \
+    | fast-p \
+    | fzf --read0 --reverse -e -d $'\t'  \
+        --preview-window down:80% --preview '
+            v=$(echo {q} | tr " " "|"); 
+            echo -e {1}"\n"{2} | grep -E "^|$v" -i --color=always;
+        ' \
+    | cut -z -f 1 -d $'\t' | tr -d '\n' | xargs -r --null $open > /dev/null 2> /dev/null
+}
