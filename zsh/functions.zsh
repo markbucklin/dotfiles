d958b1f781466f74b10b76f901b75e693f13bd96
Mark Bucklin <markbucklin@gmail.com>
2019-06-18 15:17:07 -0400

    fix functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -212,12 +212,12 @@ function printHookFunctions () {
 
 # reloads all functions
 # http://www.zsh.org/mla/users/2002/msg00232.html
-r() {
-    local f
-    f=(~/.config/zsh/functions.d/*(.))
-    unfunction $f:t 2> /dev/null
-    autoload -U $f:t
-}
+# reload_functions() {
+#     local f
+#     f=(~/.config/zsh/functions.d/*(.))
+#     unfunction $f:t 2> /dev/null
+#     autoload -U $f:t
+# }
 
 # activates zmv
 autoload zmv
